1431a6004e1a258d89a88a6059091df2d1f3064a
franqoramirez <92063944+franqoramirez@users.noreply.github.com>
2021-10-18 20:33:40 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+let contador,nota,suma,promedio: number;
+
+contador = 1;
+suma = 0;
+
+while(contador) <= 10);
+console.log ("la nota numero ", contador," ,es:",nota);
+suma = suma+nota;
+contador = contador +1;
+promedio= suma / 10;
+console.log("el promedio es:", promedio);
+
